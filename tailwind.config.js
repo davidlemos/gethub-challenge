900883fe76730b4223f83f90c6f7e267c8490d67
David Lemos <david.lemos@outlook.com>
2022-12-03 16:29:48 -0300

    inicialização

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.tsx"
+  ],
+  theme: {
+    extend: {
+      fontFamily:{
+        sans: 'Nunito, sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
